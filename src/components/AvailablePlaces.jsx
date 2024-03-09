f2d8c21cd2e980918e6d12681851b6da58d64ac0
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-03-09 20:54:06 +0200

    Added an error popup try catch to handle errors delt with loading text, to show when the places are still loading in case of slow internet

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,23 +1,39 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+  const [isLoading, setisLoading] = useState(true);
   useEffect(() => {
     async function fetchPlces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          //failed
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      }
+      setisLoading(false);
     }
     fetchPlces();
   }, []);
-
+  if (error) {
+    return <Error></Error>;
+  }
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
-      isLoading={true}
+      isLoading={isLoading}
       loadingText="Fetching Place data..."
       onSelectPlace={onSelectPlace}
     />
