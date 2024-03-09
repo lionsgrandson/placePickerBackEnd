2fc8ed6c90bcabca13cfd99f0323098d8eb698bb
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-03-09 22:19:25 +0200

    added the geo location for sorting the palces

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -16,7 +17,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
           //failed
           throw new Error("Failed to fetch places");
         }
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setisLoading(false);
+        });
       } catch (error) {
         console.log(error);
         setError({
